34da31d9f95dc122b9aff4dc8cdec27dc8851e21
Antony <jhonfagundez@gmail.com>
2021-02-06 20:23:57 -0300

    Add Commands and Relationships

--- GraphQLApp/GraphQLApp.Web/GraphQL/Query.cs
+++ GraphQLApp/GraphQLApp.Web/GraphQL/Query.cs
@@ -10,9 +10,17 @@ namespace GraphQLApp.GraphQL
     public class Query
     {
         [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
         public IQueryable<Platform> GetPlatform([ScopedService] AppDbContext context)
         {
             return context.Platforms;
+        }
+
+        [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
+        public IQueryable<Command> GetCommands([ScopedService] AppDbContext context)
+        {
+            return context.Commands;
         }
     }
 
